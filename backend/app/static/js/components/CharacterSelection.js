dbdd3e5dcebb99e8c146ae9452a5a8017d384d6c
serg aparri <sergaparri@gmail.com>
2026-01-29 09:35:31 +0800

    hihi

--- backend/app/static/js/components/CharacterSelection.js
+++ backend/app/static/js/components/CharacterSelection.js
@@ -57,11 +57,11 @@ class CharacterSelection {
 
 getCharacterIcon(characterType) {
     const icons = {
-        "Volta": '<img src="/static/images/volta.png" alt="Volta" class="character-icon-img">',
-        "Pedro Penduko": '<img src="/static/images/pedro-penduko.png" alt="Pedro Penduko" class="character-icon-img">',
-        "Kidlat": '<img src="/static/images/kidlat.png" alt="Kidlat" class="character-icon-img">',
-        "Victor Magtanggol": '<img src="/static/images/victor-magtanggol.png" alt="Victor Magtanggol" class="character-icon-img">',
-        "WaPakMan": '<img src="/static/images/wapakman.png" alt="WaPakMan" class="character-icon-img">'
+        "Volta": '<img src="/static/icons/volta.png" alt="Volta" class="character-icon-img">',
+        "Pedro Penduko": '<img src="/static/icons/pedro.png" alt="Pedro Penduko" class="character-icon-img">',
+        "Kidlat": '<img src="/static/icons/kidlat.png" alt="Kidlat" class="character-icon-img">',
+        "Victor Magtanggol": '<img src="/static/icons/victor.png" alt="Victor Magtanggol" class="character-icon-img">',
+        "WaPakMan": '<img src="/static/icons/wapakman.png" alt="WaPakMan" class="character-icon-img">'
     };
     return icons[characterType] || '👤';
   }
